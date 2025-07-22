a9c9bbca7aa33a6b7573294c3572352455907c04
Jonathas Sardinha <jonathas.sardinha@gmail.com>
2025-07-22 16:04:08 -0300

    feat: get first version of controlled Editor working

--- src/app/MonacoEditor.tsx
+++ src/app/MonacoEditor.tsx
@@ -1,21 +1,89 @@
 "use client";
 
-import { Editor } from "@monaco-editor/react";
+import {
+  Editor,
+  useMonaco,
+  type Monaco,
+  type OnMount,
+} from "@monaco-editor/react";
+import { type editor, type ISelection, Uri } from "monaco-editor";
+import { useEffect, useRef, useState } from "react";
+
+const initialModel = `console.log('Hello World')
+
+console.log('Hello World')
+
+console.log('Hello World')
+`;
 
 export function MonacoEditor() {
+  const [model, setModel] = useState<string>(initialModel);
+  const [selections, setSelections] = useState<ISelection[]>([
+    {
+      positionLineNumber: 1,
+      positionColumn: 1,
+      selectionStartLineNumber: 1,
+      selectionStartColumn: 1,
+    },
+  ]);
+  const [editor, setEditor] = useState<editor.IStandaloneCodeEditor | null>(
+    null,
+  );
+  const [monaco, setMonaco] = useState<Monaco | null>(null);
+
+  const updateEditor = () => {
+    if (model !== editor?.getValue()) {
+      editor?.setValue(model);
+    }
+
+    if (Array.isArray(selections)) {
+      if (
+        !monaco?.Selection.selectionsArrEqual(
+          selections,
+          editor?.getSelections() ?? [],
+        )
+      ) {
+        console.debug(selections, editor?.getSelections());
+        editor?.setSelections(selections);
+      }
+    }
+  };
+
+  const handleOnMount: OnMount = (mountedEditor, mountedMonaco) => {
+    setEditor(mountedEditor);
+    setMonaco(mountedMonaco);
+    updateEditor();
+
+    mountedEditor.onDidChangeCursorSelection((e) => {
+      setSelections([e.selection, ...e.secondarySelections]);
+      updateEditor();
+    });
+  };
+
   return (
     <Editor
       className="h-full w-full grow"
+      path="default.ts"
       wrapperProps={{
         className: "w-full grow flex flex-col",
       }}
       language="typescript"
-      defaultValue="// Hello World"
+      defaultValue={model}
+      value={model}
       theme="vs-dark"
       options={{
         minimap: {
           enabled: false,
         },
+        value: model,
+      }}
+      onMount={handleOnMount}
+      onChange={(value, event) => {
+        if (event.changes.every(({ range }) => range.startLineNumber > 2)) {
+          setModel(value ?? "");
+        }
+
+        setTimeout(() => updateEditor(), 0);
       }}
     />
   );
